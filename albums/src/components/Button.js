b43eb2b0024a9663ea41c50ef1696b30b73cde16
Natasha Robarge <robarge.natasha7@gmail.com>
2017-12-04 14:59:26 -0600

    Added button styling

--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
-  return(
-    <Text>Click here!</Text>
+  const { buttonStyle, textStyle } = styles;
+
+  return (
+    <TouchableOpacity onPress={} style={buttonStyle}>
+      <Text style={textStyle}>Click here!</Text>
+    </TouchableOpacity>
   );
+};
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
 };
 
 export default Button;
