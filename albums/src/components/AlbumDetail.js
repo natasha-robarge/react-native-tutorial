9ce0113ba78c41b31ceeb699688d65f52da20287
Natasha Robarge <robarge.natasha7@gmail.com>
2017-12-04 14:35:49 -0600

    Added album photo, made app scrollable

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
   return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View style={styles.imgContainerStyle}>
+          <Image 
+            style={styles.thumbnailStyle}
+            source={{ uri: thumbnail_image }} 
+          />
+        </View>
+        <View style={styles.headerContent}>
+          <Text style={styles.titleStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image 
+          style={styles.imageStyle}
+          source={{ uri: image }} 
+        />
+      </CardSection>
+    </Card>
   );
 }; 
+
+const styles = {
+  headerContent: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  titleStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  imgContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
 
 export default AlbumDetail;
