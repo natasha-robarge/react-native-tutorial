bf5e98d8ee3b749dc9d62ca2f55066015e9d7f3d
Natasha Robarge <robarge.natasha7@gmail.com>
2017-12-04 08:33:11 -0600

    exported header component and imported to index

--- albums/src/components/Header.js
+++ albums/src/components/Header.js
@@ -1,5 +1,11 @@
 // Import dependencies
+import React from 'react';
+import { Text } from 'react-native';
 
 // Make component
+const Header = () => {
+  return <Text>Albums</Text>;
+};
 
 // Export component
+export default Header;
